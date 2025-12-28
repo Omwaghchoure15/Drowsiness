0958158cbb70a2302a05b881c43b3b97acf040ff
Om_15 <omwaghchoure15@gmail.com>
2025-12-28 17:46:15 +0530

    Last push

--- app/src/main/java/com/example/ddos/data/Apiclient.kt
+++ app/src/main/java/com/example/ddos/data/Apiclient.kt
@@ -1,6 +1,5 @@
 package com.example.ddos.data
 
-<<<<<<< HEAD
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -21,21 +20,5 @@ object ApiClient {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(DrowsinessApi::class.java)
-=======
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-object ApiClient {
-    private const val BASE_URL = ""
-
-
-    val api: DrowsinessApi by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(DrowsinessApi::class.java)
-
->>>>>>> 8d198eecdf3bf42f79ee7773e982fd9283528a2a
     }
 }
